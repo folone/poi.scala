b934ee522b13cc2bb813a290649fd5b538fa08eb
Georgii Leontiev <g.leontiev@thenewmotion.com>
2011-09-20 14:32:33 +0300

    First commit. It now can only create a new xls file.

---
+++ src/main/scala/info/folone/scala/poi/Workbook.scala
@@ -0,0 +1,57 @@
+import org.apache.poi._
+import hssf.usermodel._
+import java.io.{File,FileOutputStream}
+
+package info.folone.scala.poi {
+
+  class Workbook(sheets: List[Sheet]) {
+    def save(path: String) = {
+      val workbook = new HSSFWorkbook
+      sheets foreach {sh =>
+        sh match {
+          case Sheet((name),(rows)) => {
+            val sheet = workbook createSheet name
+            rows foreach { rw =>
+              rw match {
+                case Row((index),(cells)) => {
+                  val row = sheet createRow index
+                  cells foreach { cl =>
+                    cl match {
+                      case Cell(index,data) => {
+                        val cell = row createCell index
+                        cell setCellValue data
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      val file = new FileOutputStream(new File(path))
+      workbook.write(file)
+      file.close()
+    }
+  }
+
+  object Workbook {
+    def apply(sheets: List[Sheet]) = new Workbook(sheets)
+  }
+
+  class Sheet(nm: String)(rw: List[Row]) {
+    val (name, rows) = (nm, rw)
+  }
+  object Sheet {
+    def apply(name: String)(rows: List[Row]) = new Sheet(name)(rows)
+    def unapply(sheet: Sheet) = Some((sheet.name),(sheet.rows))
+  }
+  class Row(idx: Int)(cl: List[Cell]) {
+    val (index, cells) = (idx, cl)
+  }
+  object Row {
+    def apply(index: Int)(cells: List[Cell]) = new Row(index)(cells)
+    def unapply(row: Row) = Some((row.index),(row.cells))
+  }
+  case class Cell(index: Int, data: String)
+}
---
+++ src/test/scala/info/folone/scala/poi/PoiSpec.scala
@@ -0,0 +1,26 @@
+import org.specs2.mutable._
+package info.folone.scala.poi {
+  class PoiSpec extends SpecificationWithJUnit {
+    "Poi" should {
+      "create workbook" in {
+        //cell must not beNull
+        Workbook {
+          Sheet("name") {
+            Row(1) {
+              Cell(1, "data") :: Cell(2, "data2") :: Nil
+            } ::
+            Row(2) {
+              Cell(1, "data") :: Cell(2, "data2") :: Nil
+            } :: Nil
+          } ::
+          Sheet("name2") {
+            Row(2) {
+              Cell(1, "data") :: Cell(2, "data2") :: Nil
+            } :: Nil
+          } :: Nil
+        }.save("/home/georgii/ok.xls")
+        "ok" must not be null
+      }
+    }
+  }
+}
