19858ee1a27761484b4ebeee56643524e7193534
Georgii Leontiev <g.leontiev@thenewmotion.com>
2012-09-06 17:48:51 +0300

    removed local repo from configs

--- project/Build.scala
+++ project/Build.scala
@@ -24,7 +24,6 @@ object Build extends Build {
   ) ++ externalPom()
 
   lazy val repoSettings = Seq(
-    resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
     resolvers += "Releases"  at "http://nexus.thenewmotion.com/content/repositories/releases",
     resolvers += "Snapshots" at "http://nexus.thenewmotion.com/content/repositories/snapshots"
   )
