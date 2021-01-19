f46d037007fe041faa686b4b952e473d3d68be67
kenji yoshida <6b656e6a69@gmail.com>
2021-01-19 10:50:12 +0900

    use length instead of size. remove parentheses

--- src/main/scala/info.folone/scala.poi/Workbook.scala
+++ src/main/scala/info.folone/scala.poi/Workbook.scala
@@ -26,7 +26,7 @@ class Workbook(val sheetMap: Map[String, Sheet], format: WorkbookVersion = HSSF)
     cell match {
       case StringCell(index, data) =>
         poiCell.setCellValue(data)
-        val cellHeight = (data.split("\n").size) * defaultRowHeight
+        val cellHeight = data.split("\n").length * defaultRowHeight
         if (cellHeight > row.getHeight)
           row setHeight cellHeight.asInstanceOf[Short]
       case BooleanCell(index, data) => poiCell.setCellValue(data)
