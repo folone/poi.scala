f1e2d7a0ae674bd5ca4ad5c47c363281fc51f80e
George Leontiev <george.leontiev@reddit.com>
2025-07-25 18:11:12 +0000

    Fix the tests in scala 3

--- src/test/scala/info/folone/scala/poi/WorkbookSpec.scala
+++ src/test/scala/info/folone/scala/poi/WorkbookSpec.scala
@@ -50,8 +50,10 @@ class WorkbookSpec extends Specification {
       val wb2 = Workbook(Set(sheet))
       val wb3 = Workbook(Set(Sheet("Different")(Set.empty[Row])))
 
-      (wb1 === wb2) must beEqualTo(true)
-      (wb1 === wb3) must beEqualTo(false)
+      import scalaz.Equal
+      val equalInstance = implicitly[Equal[Workbook]]
+      equalInstance.equal(wb1, wb2) must beEqualTo(true)
+      equalInstance.equal(wb1, wb3) must beEqualTo(false)
     }
 
     "work with complex sheets" in {
